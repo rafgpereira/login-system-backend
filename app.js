2d82f2f685da6c72f118ddc05b774e22d22f15f0
rafgpereira <rafaelgomespereira123@gmail.com>
2024-01-22 16:29:36 -0300

    new enviroment variables

--- app.js
+++ app.js
@@ -199,15 +199,12 @@ app.put('/user/:id', checkToken, async (req, res) => {
 
 //Credenciais
 
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
-
 mongoose
     .connect(
-        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.gulwnoc.mongodb.net/?retryWrites=true&w=majority`
+        process.env.DB_CONNECT
     )
     .then(()=>{
-        console.log('Conectado ao banco!')
+        console.log('Conectado ao banco de dados!')
     })
     .catch(err=>console.log(err))
 
